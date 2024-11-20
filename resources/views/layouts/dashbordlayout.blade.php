17700ae29fd5abca303a9518a0151f58521f9773
Khaled tarek <khaled.tarek@future-group.com>
2024-11-20 17:21:28 +0200

    docker

--- resources/views/layouts/dashbordlayout.blade.php
+++ resources/views/layouts/dashbordlayout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mail Reader</title>
+    <title>Loodz el gen</title>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <!-- Include Bootstrap CSS (Ensure this is loaded first) -->
